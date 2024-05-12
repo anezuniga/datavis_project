cecf84ff07d9640ab2f6fb91a5df9d545a0b5e25
Ane Zuniga <anezu@mit.edu>
2024-05-12 18:07:31 -0400

    4 towns update text

--- interactive_towns.js
+++ interactive_towns.js
@@ -8,15 +8,15 @@ document.getElementById('leftImage').addEventListener('click', function(event) {
 
     if (xPercentage >= 10 && xPercentage <= 30 && yPercentage >= 30 && yPercentage <= 50) {
         document.getElementById('rightImage').src = 'figures/Bolton.svg';
-        document.getElementById('rightText').textContent = 'Bolton was chosen because it is a 100% single family zoning municipality. However, Bolton has no environmental justice population.';
+        document.getElementById('rightText').textContent = 'Bolton is labeled as a municipality with 100% single-family zoning. However, Bolton does not have an environmental justice population.';
     } else if (xPercentage >= 40 && xPercentage <= 50 && yPercentage >= 50 && yPercentage <= 70) {
         document.getElementById('rightImage').src = 'figures/Newton.svg';
-        document.getElementById('rightText').textContent = 'Newton was chosen because it is a 77% single-family zoning municipality and according to analysis and the map there are a lot more single-family residences in Newton which is zoned for 77% for single-family zoning than Hanover and Bolton that have 100% single-family zoning.';
+        document.getElementById('rightText').textContent = 'Newton is a municipality with 77% single-family zoning. According to analysis and the map, there are significantly more single-family residences in Newton, which is zoned for 77% single-family zoning, compared to Hanover and Bolton, which have 100% single-family zoning.';
     } else if (xPercentage >= 50 && xPercentage <= 60 && yPercentage >= 40 && yPercentage <= 60) {
         document.getElementById('rightImage').src = 'figures/Cambridge.svg';
-        document.getElementById('rightText').textContent = 'Cambridge was chosen because it is a 0% single family zoning municipality but there are a lot of single family parcels and a lot of them are concentrated around Harvard where there are no environmental justice populations. However, Cambridge does have a lot of environmental justice population compared to municipalities that have single family zoning.';
+        document.getElementById('rightText').textContent = 'Cambridge is labeled as a 0% single-family municipality, but it actually has numerous single-family parcels, especially around Harvard, where there are no environmental justice populations. However, Cambridge does have a substantial environmental justice population compared to municipalities with single-family zoning.';
     } else if (xPercentage >= 60 && xPercentage <= 80 && yPercentage >= 70 && yPercentage <= 90) {
         document.getElementById('rightImage').src = 'figures/Hanover.svg';
-        document.getElementById('rightText').textContent = 'Hanover was chosen because it is a 100% single family zoning municipality. However, Hanover has a small portion of the environmental justice population.';
+        document.getElementById('rightText').textContent = 'Hanover is a municipality with 100% single-family zoning. However, Hanover does have a small portion of the environmental justice population.';
     }
 });
